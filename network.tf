4bc418353b55b58338564ade7157f5049efb69dd
bathinapullarao <33596044+bathinapullarao@users.noreply.github.com>
2021-02-03 10:20:34 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,12 +1,12 @@
-resource "google_compute_network" "gcm_network" {
+resource "google_compute_network" "mcm_network" {
   name = "${var.network}"
   auto_create_subnetworks = "false"
 }
 
-resource "google_compute_subnetwork" "gcm_network_subnetwork" {
+resource "google_compute_subnetwork" "mcm_network_subnetwork" {
   name          = "${var.network}-subnetwork-${var.subnetwork-region}"
   ip_cidr_range = "10.0.0.0/16"
-  network       = "${google_compute_network.gcm_network.self_link}"
-  depends_on = ["google_compute_network.gcm_network"]
+  network       = "${google_compute_network.mcm_network.self_link}"
+  depends_on = ["google_compute_network.mcm_network"]
   region        = "${var.subnetwork-region}"
 }
